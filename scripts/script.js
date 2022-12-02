b93996919312f56ff78025a8b218dd650da128dc
nrsgfun <115941984+nrsgfun@users.noreply.github.com>
2022-12-02 17:43:38 -0500

    updates week 8

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+	Student Name: Randy Rusch
+	File Name: script.js
+	Date: 11/26/2022
+*/
+
+	//Hamburger menu function
+	function hamburger() {
+		var menu = document.getElementById("menu-links");
+		var logo = document.getElementById("ffc-logo")
+		if (menu.style.display === "block" && logo.style.display === "none") {
+			menu.style.display = "none";
+			logo.style.display = "block";
+		} else {
+			menu.style.display = "block";
+			logo.style.display = "none";
+		}
+	}
